9a07f71f884144176fd7f39d62780f217bf9a0f3
HabiburRahmanAnik <55406698+habiburrahman26@users.noreply.github.com>
2022-12-15 22:40:20 +0600

    complete page header  design

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,16 @@
+import { Box } from '@mui/system';
 import React from 'react';
+import Header from '../../components/Header';
 import TopBar from '../global/TopBar';
 
 const Dashboard = () => {
-  return <p>dashboard</p>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subTitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
