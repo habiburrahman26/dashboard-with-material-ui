0eaafa2b1699b3490e592a96077f1c80911c1ce6
HabiburRahmanAnik <55406698+habiburrahman26@users.noreply.github.com>
2022-12-19 22:36:15 +0600

    contact form design complete

--- src/scenes/form/Form.jsx
+++ src/scenes/form/Form.jsx
@@ -55,14 +55,7 @@ const Form = () => {
           /* and other goodies */
         }) => (
           <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(0,1fr)"
-              sx={{
-                '& > div': { gridColumn: isMobile ? 'span 4' : undefined },
-              }}
-            >
+            <Box display="grid" gap="30px" gridTemplateColumns="repeat(0,1fr)">
               <TextField
                 fullWidth
                 variant="filled"
@@ -76,6 +69,81 @@ const Form = () => {
                 helperText={touched.firstName && errors.firstName}
                 sx={{
                   gridColumn: 'span 2',
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name="lastName"
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{
+                  gridColumn: 'span 2',
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{
+                  gridColumn: 'span 4',
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Contact"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contact}
+                name="contact"
+                error={!!touched.contact && !!errors.contact}
+                helperText={touched.contact && errors.contact}
+                sx={{
+                  gridColumn: 'span 4',
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address1"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address1}
+                name="address1"
+                error={!!touched.address1 && !!errors.address1}
+                helperText={touched.address1 && errors.address1}
+                sx={{
+                  gridColumn: 'span 4',
+                }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="address2"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address2}
+                name="address2"
+                error={!!touched.address2 && !!errors.address2}
+                helperText={touched.address2 && errors.address2}
+                sx={{
+                  gridColumn: 'span 4',
                 }}
               />
             </Box>
